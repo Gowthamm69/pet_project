561e71e61c77996486cb840e6f8c27026e4fe49d
jaya sree <kemburijayasree1@gmail.com>
2025-07-04 12:44:00 +0530

    Update vit.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
+  base: "/", // leave as "/" unless hosting in subfolder
+   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+})
